765ac7f7aa7a4ff41291848822db18a145ae96f5
Ross Lawley <ross.lawley@gmail.com>
2022-07-22 10:52:49 +0100

    Spotless code formatting configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,52 @@
+/*
+ * Copyright 2008-present MongoDB, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 group = "org.mongodb"
+
 version = "1.0-SNAPSHOT"
 
 plugins {
     kotlin("multiplatform") version "1.7.0"
+
+    id("com.diffplug.spotless") version "6.8.0"
 }
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 kotlin {
     jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
+        compilations.all { kotlinOptions.jvmTarget = "1.8" }
         withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
+        testRuns["test"].executionTask.configure { useJUnitPlatform() }
     }
 
-    js(IR) {
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
-        }
-    }
+    js(IR) { browser { commonWebpackConfig { cssSupport.enabled = true } } }
 
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    val nativeTarget =
+        when {
+            hostOs == "Mac OS X" -> macosX64("native")
+            hostOs == "Linux" -> linuxX64("native")
+            isMingwX64 -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
 
     sourceSets {
         val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
-            }
-        }
+        val commonTest by getting { dependencies { implementation(kotlin("test")) } }
         val jvmMain by getting
         val jvmTest by getting
 
@@ -52,5 +55,43 @@ kotlin {
 
         val nativeMain by getting
         val nativeTest by getting
+    }
+}
+
+spotless {
+    java {
+        googleJavaFormat("1.12.0")
+        importOrder("kotlin", "org", "com", "")
+        removeUnusedImports() // removes any unused imports
+        trimTrailingWhitespace()
+        endWithNewline()
+        indentWithSpaces()
+        licenseHeaderFile(rootProject.file("config/mongodb.license"))
+    }
+
+    kotlinGradle {
+        ktfmt("0.39").dropboxStyle()
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+        licenseHeaderFile(
+            rootProject.file("config/mongodb.license"),
+            "(group|plugins|import|buildscript|rootProject)")
+    }
+
+    kotlin {
+        target("**/*.kt")
+        ktfmt("0.39").dropboxStyle()
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
+        licenseHeaderFile(rootProject.file("config/mongodb.license"))
+    }
+
+    format("extraneous") {
+        target("*.xml", "*.yml", "*.md")
+        trimTrailingWhitespace()
+        indentWithSpaces()
+        endWithNewline()
     }
 }
