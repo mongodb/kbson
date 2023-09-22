2a5049b8d0956411fcc165ce8e331324fe5fd688
Claus Rørbech <claus.rorbech@gmail.com>
2023-09-22 15:58:59 +0200

    Parse all string literals into BsonStrings (#17)

--- src/commonMain/kotlin/org/mongodb/kbson/serialization/Serializers.kt
+++ src/commonMain/kotlin/org/mongodb/kbson/serialization/Serializers.kt
@@ -379,6 +379,9 @@ internal object BsonValueSerializer : KSerializer<BsonValue>, BsonSerializer {
                 return bsonArray
             }
             is JsonPrimitive -> {
+                if (jsonElement.isString) {
+                    return BsonString(jsonElement.content)
+                }
                 jsonElement.booleanOrNull?.let {
                     return BsonBoolean(it)
                 }
